f07266732433672209fb5f08d0997e7d61f0b4ce
Justin Campbell <justin@justincampbell.me>
2013-07-10 11:30:22 -0400

    Create Shortener

---
+++ shortener.py
@@ -0,0 +1,15 @@
+class Shortener(object):
+
+  def __init__(self):
+    self.id = 0
+    self.urls = {}
+
+  def shorten(self, url):
+    self.id += 1
+    token = str(self.id)
+    self.urls[token] = url
+    return token
+
+  def expand(self, token):
+    return self.urls[token]
+
---
+++ shortener_test.py
@@ -0,0 +1,23 @@
+from shortener import Shortener
+import unittest
+
+class TestShortener(unittest.TestCase):
+
+    url = "http://example.com"
+
+    def setUp(self):
+        self.shortener = Shortener()
+
+    def test_init_with_clean_slate(self):
+        self.assertEqual(self.shortener.id, 0)
+        self.assertEqual(self.shortener.urls, {})
+
+    def test_shorten_url_and_expand_token(self):
+        token = self.shortener.shorten(TestShortener.url)
+        expanded_url = self.shortener.expand(token)
+        self.assertEqual(TestShortener.url, expanded_url)
+        self.assertNotEqual(TestShortener.url, token)
+        self.assertNotEqual(token, self.shortener.shorten("http://another.com"))
+
+if __name__ == '__main__':
+    unittest.main()
